81eb36526f85b44ca5d722e80afb1e2bb21a62fc
Jameka Echols <jameka.echols@gmail.com>
2021-03-07 22:14:15 -0600

    added auth and logout

---
+++ Twitter/HomeTableTableViewController.swift
@@ -0,0 +1,39 @@
+//
+//  HomeTableTableViewController.swift
+//  Twitter
+//
+//  Created by Jameka Echols on 3/7/21.
+//  Copyright © 2021 Dan. All rights reserved.
+//
+
+import UIKit
+
+class HomeTableTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    @IBAction func OnLogout(_ sender: Any) {
+        TwitterAPICaller.client?.logout()
+        
+        //screen will dismiss; disapper
+        self.dismiss(animated: true, completion:nil)
+    }
+    
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }
+
+    
+
+}
